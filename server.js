abd143be8f7a11a8fe0d27befa250954dc549660
aquariax <roberthrrs8@gmail.com>
2020-11-24 23:27:04 -0500

    set up users and auth routes and created middle ware function for protected routes

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 
+connectDB();
 const PORT = process.env.Port || 5000;
+
+app.use(express.json({ extended: false }));
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
