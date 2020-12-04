e65238ebcb663fae00278f7506cfd33f0f49c020
aquariax <roberthrrs8@gmail.com>
2020-12-03 23:44:50 -0500

    created forms and contact components and enabled ability to add contacts

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,61 @@
+import React, { useReducer } from 'react';
+import { v4 } from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+	ADD_CONTACT,
+	DELETE_CONTACT,
+	SET_CURRENT,
+	CLEAR_CURRENT,
+	UPDATE_CONTACT,
+	FILTER_CONTACTS,
+	CLEAR_FILTER,
+} from '../types';
+
+const ContactState = (props) => {
+	const initialState = {
+		contacts: [
+			{
+				id: 1,
+				name: 'Joe Rogan',
+				email: 'email2@email.com',
+				phone: '444-444-4444',
+				type: 'professional',
+			},
+			{
+				id: 2,
+				name: 'Walter Rogan',
+				email: 'email3@email.com',
+				phone: '644-444-4444',
+				type: 'personal',
+			},
+			{
+				id: 3,
+				name: 'Victor Rogan',
+				email: 'email4@email.com',
+				phone: '544-444-4444',
+				type: 'professional',
+			},
+		],
+	};
+
+	const [state, dispatch] = useReducer(contactReducer, initialState);
+
+	const addContact = (contact) => {
+		contact.id = v4();
+		dispatch({ type: ADD_CONTACT, payload: contact });
+	};
+
+	return (
+		<ContactContext.Provider
+			value={{
+				contacts: state.contacts,
+				addContact,
+			}}
+		>
+			{props.children}
+		</ContactContext.Provider>
+	);
+};
+
+export default ContactState;
