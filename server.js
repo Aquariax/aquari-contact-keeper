93b3179efa578412c64c78d86262725f2c6d2244
aquariax <roberthrrs8@gmail.com>
2020-12-08 06:55:30 -0500

    fixed getting /

--- server.js
+++ server.js
@@ -18,6 +18,4 @@ if (process.env.NODE_ENV === 'production') {
 	);
 }
 
-app.get('/', (req, res) => res.json({ msg: 'Hi there' }));
-
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
