e360d96270c17630cfc103b3b72641496e96387c
aquariax <roberthrrs8@gmail.com>
2020-12-08 07:05:24 -0500

    testing program

--- server.js
+++ server.js
@@ -4,18 +4,16 @@ const app = express();
 const path = require('path');
 connectDB();
 const PORT = process.env.Port || 5000;
-
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 app.use(express.json({ extended: false }));
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'PRODUCTION') {
 	app.use(express.static('client/build'));
 	app.get('*', (req, res), (res) =>
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 	);
 }
-
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
